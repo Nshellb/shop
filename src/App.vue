e6b79a0621a985658b57d5eabfdd69ab9101f255
Nshell <nshellb@gmail.com>
2021-08-01 07:11:57 +0900

    08012021 [Vue.js] 템플릿을 활용한 Vue.js 개발 - 2. 템플릿 구조 통화변경 select box와 장바구니 미동작 수정

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,18 @@
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
-export default{
+export default {
   components: {
     Header,
     Footer,
-  }
+  },
+  mounted() {
+    $(".selection-1").select2({
+      minimumResultsForSearch: 20,
+      dropdownParent: $('#dropDownSelect1')
+    });
+
+    $.initialize();
+  },
 }
 </script>
