dd98a46986fcf73d68e12a46c3746a62684d4e2d
Nshell <nshellb@gmail.com>
2021-08-02 06:02:50 +0900

    08022021 [Vue.js] 템플릿을 활용한 Vue.js 개발 - 7. Featured product 영역 구현하기 Featured product data 및 layout 출력 완료

--- src/api/product.js
+++ src/api/product.js
@@ -4,4 +4,7 @@ export default {
     async getBestProducts() { // best-product를 가져오는 코드 
         return http.get('api/best-product.json');
     },
+    async getFeaturedProducts() { // featured-product를 가져오는 코드
+        return http.get('api/featured-product.json');
+    },
 }
--- src/store/product/index.js
+++ src/store/product/index.js
@@ -3,18 +3,27 @@ import productApi from '@/api/product';
 export default {
     namespaced: true,
     state: {
-        bestProducts: []
+        bestProducts: [],
+        featuredProducts: [],
     },
     mutations: {
         setBestProducts(state, products) {
             state.bestProducts = [].concat(products); // 3. state에 저장하도록 만들어준다.
         },
+        setFeaturedProducts(state, products) {
+            state.featuredProducts = [].concat(products);
+        }
     },
     actions: {
         async setBestProducts({ commit }) {
             const response = await productApi.getBestProducts(); // 1. getBestProducts를 api 에서 가져와서 
 
             commit('setBestProducts', response.data); // 2. mutation을 호출하고 
+        },
+        async setFeaturedProducts({ commit }) {
+            const response = await productApi.getFeaturedProducts(); 
+
+            commit('setFeaturedProducts', response.data); 
         },
     },
 }
